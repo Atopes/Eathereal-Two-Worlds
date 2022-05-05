f353caf36b93f23ae445b93a34d271c949c395df
Marek324 <hrmajo1@gmail.com>
2022-05-05 14:21:48 +0200

    Update UIController.cs

--- Ethereal Two Worlds Git/Assets/Scripts/UI/UIController.cs
+++ Ethereal Two Worlds Git/Assets/Scripts/UI/UIController.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 using TMPro;
 
 public class UIController : MonoBehaviour
